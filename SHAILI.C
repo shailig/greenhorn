f8a733998f26af25e691b758e51b0dc8ce8072f3
shailig <132878897+shailig@users.noreply.github.com>
2023-05-08 12:28:57 +0530

    Add files via upload

---
+++ SHAILI.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+
+   int a=10,b=51,c;
+
+   clrscr();
+
+   c=a-b;
+   printf("Addition of A-B=%d",c);
+   c=a+b;
+   printf("substration of A+B=%d",c);
+   c=a*b;
+   printf("nmultipliction of a*b=%d",c);
+   c=a/b;
+   printf("\ndivison of  a/b=%d",c);
+   c=a%b;
+   printf("\nmodulas of a%b=%d",c);
+   getch();
+}
+
+
+
+
